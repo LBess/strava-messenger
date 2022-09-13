4f393496f5562a896822f7baf98ccd2e49cd058d
Liam Bessell <lbessell7@gmail.com>
2022-09-12 21:45:41 -0600

    Add SNS

--- strava_messenger/app.py
+++ strava_messenger/app.py
@@ -2,19 +2,32 @@ import json
 import boto3
 
 VERIFY_TOKEN_PARAMETER_NAME = "strava_messenger_verification_token"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:650994256587:strava-motivation"
+SUBSCRIPTION_ID = 222977
+OBJECT_TYPE_ACTIVITY = "activity"
+ASPECT_TYPE_CREATE = "create"
 
 def handleSubscriptionRequest(queryStringParameters):
     """Respond to the Strava subscription validation request
 
     Doc: https://developers.strava.com/docs/webhooks/
+
+    Parameters
+    ----------
+    queryStringParameters: dict, required
+
+    Returns
+    ------
+    HTTP Body: dict
     """
+
     print("START handle Strava subscription validation request")
     print(queryStringParameters)
 
     ssmClient = boto3.client("ssm")
     parameterResponse = ssmClient.get_parameter(Name=VERIFY_TOKEN_PARAMETER_NAME)
     verifyToken = parameterResponse["Parameter"]["Value"]
-    if (verifyToken != queryStringParameters["hub.verify_token"]):
+    if verifyToken != queryStringParameters["hub.verify_token"]:
         raise Exception("Invalid verify_token")
 
     return {
@@ -27,9 +40,30 @@ def handleActivityPost(body):
     """
     print("START handle Strava activity post")
     print(body)
+    
+    if body["subscription_id"] != SUBSCRIPTION_ID:
+        raise Exception("Invalid subscription_id")
+        
+    if body["object_type"] != OBJECT_TYPE_ACTIVITY:
+        return {
+            "message": "object_type is not activity"
+        }
+    
+    if body["aspect_type"] != ASPECT_TYPE_CREATE:
+        return {
+            "message": f"aspect_type is not {ASPECT_TYPE_CREATE}"
+        }
+
+
+    stravaActivityId = body["object_id"]
+    snsSubject = f"Strava Activity {stravaActivityId}"
+    snsMessage = "I\'m the boss of this gym."
+
+    snsClient = boto3.client("sns")
+    snsClient.publish(TopicArn=SNS_TOPIC_ARN, Message=snsMessage, Subject=snsSubject)
 
     return {
-        "message": "yo"
+        "message": "success"
     }
 
 
@@ -45,6 +79,7 @@ def handler(event, context):
     ------
     API Gateway Lambda Proxy Output Format: dict
     """
+    
     errorMessage = ""
     responseBody = {}
     try:
@@ -56,16 +91,15 @@ def handler(event, context):
             requestBody = json.loads(event["body"])
             responseBody = handleActivityPost(requestBody)
         else:
-            errorMessage = f"Error: Invalid HTTP Method {httpMethod}"
+            errorMessage = f"Invalid HTTP Method {httpMethod}"
     except Exception as e:
         if (len(e.args) > 0):
-            print(e)
-            errorMessage = f"Error: {e.args[0]}"
+            errorMessage = e.args[0]
         else:
             "An error occurred"
 
     if errorMessage != "":
-        print(errorMessage)
+        print(f"Error: {errorMessage}")
         return {
             "statusCode": 500,
             "body": json.dumps({
@@ -73,7 +107,6 @@ def handler(event, context):
             })
         }
     
-    print("SUCCESS")
     return {
         "isBase64Encoded": False,
         "statusCode": 200,
